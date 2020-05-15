0ff4f6a12d037209c173decbccf92fb07077a666
AsSerge <z00m.serge@gmai.com>
2020-05-15 18:07:41 +0300

    start

---
+++ js/translate.js
@@ -0,0 +1,102 @@
+var arrLang = {
+	'en': {
+		'menu_home': 'Home',
+		'menu_about': 'About Us',
+		'menu_who': 'Who we are',
+		'menu_service': 'Service',
+		'menu_projects': 'Projects',
+		'menu_our_team': 'Our team',
+		'menu_contacts': 'Contacts',
+		'intro_btn_more_details': 'More details',
+		'main_subtitle': 'RBMK INVEST S.R.O. Is Czech investment company formed in 2018 in Praha.',
+		'about_title': 'About US',
+		'about_p1': 'RBMK INVEST S.R.O. Is Czech investment company formed in 2018 in Praha. Company main goal-is accumulate the expertise of investment in new projects such as:',
+		'about_li1_span': 'Development',
+		'about_li2_span': 'Trade',
+		'about_li3_span': 'IT services',
+		'about_li4_span': 'Financial services',
+		'about_p2': 'The geography of the company activities is presented in such countries as: Czech Republic, Germany, Switzerland, Russia, China, Kazakhstan, Kyrgyzstan, Cyprus, Kenya.',
+		'features_title': 'Who we are',
+		'features_p1': 'Entrepreneurial. Passionate Exclusive. These are the qualities that can be used to describe the community of successful entrepreneurs and private individuals who have joined together under the RMBK investment management.',
+		'features_p2': 'The shareholders ’shared objective is to invest their own private funds directly as equity in medium-sized companies. RBMK INVEST therefore sees itself as an expert contact in all matters pertaining to our central topics of entrepreneurship, growth and finance - always on an equal footing with the target companies and the people acting on their behalf.',
+		'features_p3': 'While we look after our investor network in the Czech, the Swiss and the Germany companies, structure and manage exclusive direct investments in selected growth companies and are also responsible for acquiring and looking after investors.',
+		'call-to-action_title': 'Contact us',
+		'call-to-action_p1': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non odit natus perferendis officia autem porro ratione, aut, harum nihil officiis eveniet tempore labore doloremque? Totam est sint nihil assumenda.',
+		'call-to-action_title_btn': 'Request a cal',
+		'portfolio_title': 'Projects',
+		'portfolio_p1': 'The company investment portfolio contains all the necessary trends for doing business more diversified and stable. We are invested globally in funds, secondaries and directly in private equity.',
+		'portfolio_all': 'All',
+		'portfolio_app': 'Development',
+		'portfolio_card': 'Trade',
+		'portfolio_web': 'IT services',
+		'portfolio_fin': 'Financial services',
+		'team_title': 'Our Team',
+		'team_p1': 'We have professional team that consist form more then 7th experts in investments, finances, management and development with experience in BIG 4 of consulting companies.',
+		'place_map_title': 'How to find us',
+		'footer_subscription_title': 'Subscription',
+		'footer_contacts_title': 'Contacts',
+		'footer_contacts_addres': 'Djufanova 1096/8, Řepy, 163 00 Praha 6'
+
+	},
+	'cz': {
+		'menu_home': 'Domů',
+		'menu_about': 'O nás',
+		'menu_who': 'Kdo jsme',
+		'menu_service': 'Služba',
+		'menu_projects': 'Projekty',
+		'menu_our_team': 'Náš tým',
+		'menu_contacts': 'Kontakty',
+		'intro_btn_more_details': 'Více informací',
+		'main_subtitle': 'RBMK INVEST S.R.O. Je česká investiční společnost založená v roce 2018 v Praze.',
+		'about_title': 'O nás',
+		'about_p1': 'RBMK INVEST S.R.O. Je česká investiční společnost založená v roce 2018 v Praze. Hlavním cílem společnosti je shromažďovat odborné znalosti o investicích do nových projektů, jako jsou:',
+		'about_li1_span': 'Rozvoj',
+		'about_li2_span': 'Obchod',
+		'about_li3_span': 'IT služby',
+		'about_li4_span': 'Finanční služby',
+		'about_p2': 'Zeměpisná činnost společnosti je prezentována v těchto zemích: Česká republika, Německo, Švýcarsko, Rusko, Čína, Kazachstán, Kyrgyzstán, Kypr, Keňa.',
+		'features_title': 'Kdo jsme',
+		'features_p1': 'Podnikatelské. Vášnivý exkluzivní. To jsou vlastnosti, které lze použít k popisu komunity úspěšných podnikatelů a soukromých osob, které se spojily v rámci řízení investic RMBK.',
+		'features_p2': 'Společným cílem akcionářů je investovat vlastní soukromé zdroje přímo jako kapitál do středních společností. RBMK INVEST se proto považuje za odborný kontakt ve všech záležitostech týkajících se našich ústředních témat podnikání, růstu a financí - vždy na stejné úrovni s cílovými společnostmi a lidmi jednajícími jejich jménem.',
+		'features_p3': 'Zatímco pečujeme o naši síť investorů v českých, švýcarských a německých společnostech, strukturujeme a spravujeme exkluzivní přímé investice ve vybraných růstových společnostech a zodpovídáme také za získávání a péči o investory.',
+		'call-to-action_title': 'Kontaktujte nás',
+		'call-to-action_p1': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non odit natus perferendis officia autem porro ratione, aut, harum nihil officiis eveniet tempore labore doloremque? Totam est sint nihil předpokládá.',
+		'call-to-action_title_btn': 'Vyžádejte si kal',
+		'portfolio_title': 'Projekty',
+		'portfolio_p1': 'Investiční portfolio společnosti obsahuje všechny nezbytné trendy pro podnikání diverzifikovanější a stabilnější. Investujeme globálně do fondů, sekundárních společností a přímo do soukromého kapitálu.',
+		'portfolio_all': 'Všechno',
+		'portfolio_app': 'Rozvoj',
+		'portfolio_card': 'Obchod',
+		'portfolio_web': 'IT služby',
+		'portfolio_fin': 'Finanční služby',
+		'team_title': 'Náš tým',
+		'team_p1': 'Máme profesionální tým, který tvoří více než sedmý odborník v oblasti investic, financí, řízení a rozvoje se zkušenostmi v BIG 4 poradenských společností.',
+		'place_map_title': 'Jak nás najdete',
+		'footer_subscription_title': 'Předplatné',
+		'footer_contacts_title': 'Kontakty',
+		'footer_contacts_addres': 'Žufanova 1096/8, Řepy, 163 00 Praha 6'
+	}
+};
+// Define a cookie and set the language
+
+if ($.cookie('lang') == undefined) {
+	$.cookie('lang', 'en', {
+		expires: 7
+	});
+}
+var lang = $.cookie('lang');
+$('.lang').each(function (index, element) {
+	$(this).text(arrLang[lang][$(this).attr('key')]);
+});
+
+$(function () {
+	$('.translate').click(function () {
+		var lang = $(this).attr('id');
+		$.cookie('lang', lang, {
+			expires: 7
+		});
+		$('.lang').each(function (index, element) {
+			$(this).text(arrLang[lang][$(this).attr('key')]);
+		});
+	});
+});
